5f96e4acbb11664c6952e912645c22cc0afed5d9
Ryan Wire <ryanwire@outlook.com>
2021-07-31 23:02:17 +0300

    Solves exercise 01

---
+++ Exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            string regex;
+            do
+            {
+                Console.WriteLine("Enter a regular expression (or press ENTER to use the default): ^[a-z]+$");
+                regex = Console.ReadLine();
+                Console.WriteLine("Enter some input:");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(regex))
+                {
+                    regex = "^[a-z]+$";
+                    bool match = Regex.Match(input, regex).Success;
+                    Console.WriteLine($"{input} matches {regex}? {match}");
+                }
+                else
+                {
+                    bool match = Regex.Match(input, regex).Success;
+                    Console.WriteLine($"{input} matches {regex}? {match}");
+                }
+                Console.WriteLine("Press ESC to end or any key to try again.");
+                cki = Console.ReadKey();
+                Console.WriteLine();
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+    }
+}
