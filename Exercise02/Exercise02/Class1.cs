194b476ee76de86256c275fbd74805b0262e0d54
Ryan Wire <ryanwire@outlook.com>
2021-08-01 19:47:16 +0300

    Solves exercise 02 and 03

---
+++ Exercise02/Exercise02/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+namespace Exercise02
+{
+    public static class BigIntegerExtensions
+    {
+        public static string ConvertToWords(BigInteger value) 
+        {
+            //var bigIntegerLength = value.Length;
+
+            string[] uniqueDigits = new string[] {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine",
+                "ten", "eleven", "twelve",
+                "thirteen",  "fourteen", "fifteen", "sixteen",
+                "seventeen", "eighteen", "nineteen"
+            };
+
+            string[] tensMultiple = new string[] {
+                "", "", "twenty",  "thirty", "forty",
+                "fifty", "sixty", "seventy", "eighty", "ninety"
+            };
+
+            string[] tensPower
+                = new string[] { "hundred", "thousand" };
+
+            if (value < 20)
+            {
+                return uniqueDigits[(int)value];
+            }
+
+            // TODO: Add code to parse numbers over 20 here
+            return $"big integer in words {value}";
+        }
+        public static string ToWords(this BigInteger bigInteger)
+        {
+            return ConvertToWords(bigInteger);
+        }
+    }
+}
---
+++ Exercise02/Exercise03/Program.cs
@@ -0,0 +1,40 @@
+﻿using Exercise02;
+using System;
+using System.Numerics;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.WriteLine("Input a number to be displayed in words:");
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    if (BigInteger.TryParse(input, out BigInteger bigInteger))
+                    {
+                        var toWords = bigInteger.ToWords();
+                        Console.WriteLine($"Your number in words is: {toWords}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid input");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid input");
+                }
+                Console.WriteLine("Press ESC to end or any key to try again.");
+                cki = Console.ReadKey();
+                Console.WriteLine();
+            } while (cki.Key != ConsoleKey.Escape);
+
+        }
+    }
+}
