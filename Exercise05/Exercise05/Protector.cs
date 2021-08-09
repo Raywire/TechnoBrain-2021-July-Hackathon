a50f42f1e9bc606def0a365abed63194c7288b61
Ryan Wire <ryanwire@outlook.com>
2021-08-09 17:12:16 +0300

    Solves Exercise 06

--- Exercise05/Exercise05/Protector.cs
+++ Exercise05/Exercise05/Protector.cs
@@ -25,6 +25,24 @@ namespace Exercise05
             return Convert.ToBase64String(ms.ToArray());
         }
 
+        public static string Decrypt(string cryptoText, string password)
+        {
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt,
+            iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+            var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateDecryptor(),
+            CryptoStreamMode.Write))
+            {
+                cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+            }
+            return Encoding.Unicode.GetString(ms.ToArray());
+        }
+
+
         public static User SaltAndHashPassword (string password)
         {
             // generate a random salt
---
+++ Exercise05/Exercise06/Program.cs
@@ -0,0 +1,60 @@
+﻿using Exercise05;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using static System.IO.Path;
+
+namespace Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlFileDirectory = (GetFullPath(@"..\..\..\..\Exercise05"));
+            string xmlFilePath = Combine(xmlFileDirectory, "customers-encrypted.xml");
+
+            string encryptionPassword = "121212";
+            List<Customer> listOfUnencryptedCustomers = new List<Customer> { };
+
+            // Read encrypted xml file
+            using (FileStream fileStream = File.Open(xmlFilePath, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Customer>), new XmlRootAttribute("customers"));
+                List<Customer> customers = (List<Customer>)serializer.Deserialize(fileStream);
+
+                foreach (Customer item in customers)
+                {
+                    listOfUnencryptedCustomers.Add(new Customer()
+                    {
+                        Name = item.Name,
+                        CreditCard = Protector.Decrypt(item.CreditCard, encryptionPassword),
+                        Password = item.Password
+                    });
+                }
+            }
+
+            // Write to encrypted xml file
+            string xmlFileEncryptedPath = Combine(GetFullPath(@"..\..\..\"), "customers-unencrypted.xml");
+
+            using (XmlWriter xmlWriter = XmlWriter.Create(xmlFileEncryptedPath, new XmlWriterSettings { Indent = true, Encoding = System.Text.Encoding.UTF8 }))
+            {
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("customers");
+                foreach (Customer customer in listOfUnencryptedCustomers)
+                {
+                    xmlWriter.WriteStartElement("customer");
+                    xmlWriter.WriteElementString("name", customer.Name);
+                    xmlWriter.WriteElementString("creditcard", customer.CreditCard);
+                    xmlWriter.WriteElementString("password", customer.Password);
+                    xmlWriter.WriteEndElement();
+                }
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndDocument();
+            }
+
+            Console.WriteLine("Decryption completed");
+        }
+    }
+}
